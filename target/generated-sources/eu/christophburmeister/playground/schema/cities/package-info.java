768cfbb335a7f2804f63539efb36c49556b7f44f
Christoph Burmeister <chburmeister@googlemail.com>
2015-09-27 09:53:16 +0200

    removed target

--- target/generated-sources/eu/christophburmeister/playground/schema/cities/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2015.09.25 um 11:44:53 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://christophburmeister.eu/playground/schema/cities", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package eu.christophburmeister.playground.schema.cities;
